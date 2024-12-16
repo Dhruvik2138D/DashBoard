9b961929b94c2466ef59b47583408276a8ac8010
Dhruvik2138D <paladiyadhruvik2023.katargam@gmail.com>
2024-12-16 14:09:18 +0530

    Dashboard

---
+++ src/Compnents/TableComponent.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+import Switch from "@mui/material/Switch";
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const TableComponent = ({ TableHeader, TableData, renderRow }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow sx={{backgroundColor:'#1976D2'}}>
+            {TableHeader.map((el, i) => {
+              return <TableCell key={el} className="text-white">{el}</TableCell>;
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {TableData.map((row, index) => (
+            <TableRow
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              {renderRow(row,index)}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TableComponent;
